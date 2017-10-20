9a170513e416a2fbbfe6e8f796d4994d37c16a90
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-20 15:54:55 +0100

    Implemented navbar with dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
